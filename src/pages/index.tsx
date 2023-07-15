c9436b9b3793234d37f9329b7f0a7a06c2598522
ALINA <132771417+FrontDevInfo@users.noreply.github.com>
2023-07-15 21:24:43 +0300

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,34 +5,32 @@ import { setSearchState } from "../store/slices/searchSlice";
 import { useSelector } from "react-redux";
 
 const HomePage = () => {
-		const dispatch = useDispatch();
-		const searchState = useSelector(selectSearchState);
+	const dispatch = useDispatch();
+	const searchState = useSelector(selectSearchState);
 	const [value, setValue] = useState(searchState);
 
-		const onInputChange = (e) => {
-			setValue(e.target.value);
-			dispatch(setSearchState(e.target.value));
-		};
-
-		return (
-			<>
-				<section className="section">
-					<h1>Next and Redux</h1>
-
-					<div className="">
-						<input
-							type="text"
-							className="input"
-							value={value}
-							onChange={onInputChange}
-							placeholder="Введите что-нибудь"
-						/>
-
-						<p>searchState: {searchState}</p>
-					</div>
-				</section>
-			</>
-		);
+	const onInputChange = (e) => {
+		setValue(e.target.value);
+		dispatch(setSearchState(e.target.value));
 	};
+
+	return (
+		<section className="section">
+			<h1>Next and Redux</h1>
+
+			<div className="">
+				<input
+					type="text"
+					className="input"
+					value={value}
+					onChange={onInputChange}
+					placeholder="Введите что-нибудь"
+				/>
+
+				<p>searchState: {searchState}</p>
+			</div>
+		</section>
+	);
+};
 
 export default HomePage;
